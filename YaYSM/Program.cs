bcc618951c5b057ed24b3a9270efad296ffbef9f
Kenneth Maxwell Lundberg <max.lundberg@live.com>
2018-01-23 23:03:06 -0800

    Add project files.

---
+++ YaYSM/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace YaYSM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
